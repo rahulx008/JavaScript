d20d1b1a194b21594982316fff42697a3520b8fb
Rahul Gupta <rgcool1999@gmail.com>
2025-08-26 14:04:10 +0530

    Datatype, Conversion, typeof,  const, let, var

---
+++ test.js
@@ -0,0 +1,23 @@
+console.log("Rahul");
+
+const accountNumber =123;
+let accountName = "Rahul";
+var accountCity="Delhi";
+
+// Var is not used because of No scoping in it.
+// Type of unassigned value-- Undefined.
+
+// console.table method
+//console.table([accountNumber,accountName, accountCity])
+//typeof
+    // console.log(typeof null); // object
+    //console.log(typeof(accountCity)); // string
+
+
+/* Boolean of any string is true except empty string. Boolean of 0 is false.*/
+    // let isLoggedIn = "false";
+    // console.log(Boolean(isLoggedIn)); // true
+    // console.log(Boolean("")); // false
+    // console.log(Boolean(0)); // false
+    // console.log(Boolean(1)); // true
+
