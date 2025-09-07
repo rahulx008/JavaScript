38070dd3dc426af1dd725c538b265085a7c1d9ad
Rahul Gupta <rgcool1999@gmail.com>
2025-09-08 02:46:52 +0530

    Add function examples and comments in functions.js

---
+++ functions.js
@@ -0,0 +1,29 @@
+// Function declaration
+// function addNumbers(a,b) {
+//     return a + b;   
+// }
+
+// Function expression
+// const add = function(a, b) {
+//     return a + b;
+// }
+
+// Arrow function
+// const addArrow = (a, b) => {return a + b};   
+
+// Function constructor
+// const addConstructor = new Function('a', 'b', 'return a + b');
+// console.log(addConstructor(2, 3));    
+
+//rest operator
+/*
+    function add (val1, val2, ...value){
+        let sum=0;
+        for(let i =0;i<value.length; i++){
+            sum+=value[i];
+        }
+        return sum;
+    }
+
+    console.log(add(1,2,3,4)); // val1 =1, val 2 = 2, value=[3,4]
+*/
